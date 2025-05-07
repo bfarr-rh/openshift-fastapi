297d70f92bd187c366105578abd9228199723e93
Benjamin Farr <bfarr-rh@users.noreply.github.com>
2025-05-07 12:09:15 +1000

    added environment

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import uvicorn
 
 app = FastAPI()
 
+def get_arg(env, default):
+    return os.getenv(env) if os.getenv(env, '') is not '' else default
+    
 @app.get('/')
 def index():
     return {'data': 'Hello FastAPI!'}
 
 @app.get('/message')
 def index():
-    return {'data': 'FastAPI is great!'}
+    if get_arg('verson','') == 'v1'
+        return {'data': 'FastAPI v1 is great!'}
+    else
+        return {'data': 'FastAPI v2 is better!'}
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8080)
